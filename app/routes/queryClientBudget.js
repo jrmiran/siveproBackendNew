9f4963964d1e4f9b44d8826915e4ae5a1ae365c0
Jefferson <jeffersonrfmiranda@gmail.com>
2019-05-31 09:47:42 -0300

    make it better

--- app/routes/queryClientBudget.js
+++ app/routes/queryClientBudget.js
@@ -7,7 +7,6 @@ module.exports = function(app, budgetClient){
     app.get(`/${budgetClient}/:nameEmpresa/:nameThirdy`, function(req, res){  
         console.log(req.params.nameEmpresa)
         let sql = `Select ce.nome, ce.telefone, ce.celular, ce.email, ce.endereco, c.id from Cliente as c, ClienteEmpresa as ce where ce.nome = ${req.params.nameThirdy} and c.nome = ${req.params.nameEmpresa}`
-        //let sql = `select v.telefone, v.celular, v.email, v.id, c.id from Cliente as c, Vendedor as v where c.nome = ${req.params.nameEmpresa} and v.nome = ${req.params.nameVendor}`
         
         con.query(sql, function(err, result){
             res.send(result);
--- config/dbConnection.js
+++ config/dbConnection.js
@@ -8,18 +8,18 @@ var mysql = require('mysql');
         database: "belartte"
     });*/
     
-/*module.exports = function(){
+module.exports = function(){
     return mysql.createConnection({
         host: "db4free.net",
         user: "belartte",
         password: "mdcjjh13!@",  
         database: "belartte"
-    });*/
-    module.exports = function(){
+    });
+    /*module.exports = function(){
     return mysql.createConnection({
         host: "162.241.2.215",
         user: "belart72_belartt",
         password: "mdcjjh13!@",  
         database: "belart72_belartte"
-    });
+    });*/
 }
