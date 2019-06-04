240af6bf4b7778860d8c94a4244bc82dad8386e4
Jefferson <jeffersonrfmiranda@gmail.com>
2019-06-04 17:09:59 -0300

    make it better

--- app/routes/queryBudgetInsertion.js
+++ app/routes/queryBudgetInsertion.js
@@ -12,6 +12,7 @@ module.exports = function(app, budgetInsertion){
         let sql = `Insert into Orcamento_codigos (Orcamento_id, codigos) values ${req.params.budgetCodes}; Insert into Orcamento_comodos (Orcamento_id, comodos) values ${req.params.budgetAmbients}`;
         
         con.query(sql, function(err, result){
+            console.log(result);
             res.send(result);
         });
     });
