874681861f5896b588d482d593840d7d47c10edd
Jefferson <jeffersonrfmiranda@gmail.com>
2019-06-04 10:59:22 -0300

    make it better

--- app/routes/queryBudgetInsertion.js
+++ app/routes/queryBudgetInsertion.js
@@ -8,7 +8,7 @@ module.exports = function(app, budgetInsertion){
     app.get(`/${budgetInsertion}/:budgetCodes/:budgetAmbients`, function(req, res){  
 
         let sql = `Insert into Orcamento_codigos (Orcamento_id, codigos) values ${req.params.budgetCodes};
-                   Insert into Orcamento_comodos (Orcamento_id, comodos) values ${req.params.budgetAmbients};`;
+                   Insert into Orcamento_comodos (Orcamento_id, comodos) values ${req.params.budgetAmbients}`;
         
         con.query(sql, function(err, result){
             res.send(result);
