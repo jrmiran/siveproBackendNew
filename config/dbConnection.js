b30c791999efc70618984333ffc85c35c6c2a4e6
Jefferson <jeffersonrfmiranda@gmail.com>
2019-06-06 10:13:20 -0300

    troca de servidor mysql

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -9,21 +9,21 @@ var mysql = require('mysql');
         multipleStatements: true
     });*/
     
-    /*module.exports = function(){
+    module.exports = function(){
     return mysql.createConnection({
         host: "db4free.net",
         user: "belartte",
         password: "mdcjjh13!@",  
         database: "belartte",
         multipleStatements: true
-    });*/
+    });
     
-    module.exports = function(){
+    /*module.exports = function(){
     return mysql.createConnection({
         host: "162.241.2.215",
         user: "belart72_belartt",
         password: "mdcjjh13!@",  
         database: "belart72_belartte",
         multipleStatements: true
-    });
+    });*/
 }
