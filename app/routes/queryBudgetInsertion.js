171cfe446ef2b829e38783afffd4f3dd1ce2bb54
Jefferson <jeffersonrfmiranda@gmail.com>
2019-06-14 10:32:28 -0300

    make it better

--- app/routes/queryBudgetInsertion.js
+++ app/routes/queryBudgetInsertion.js
@@ -3,9 +3,9 @@ var dbConnection = require('../../config/dbConnection');
 module.exports = function(app, budgetInsertion){
     var con = dbConnection();
     
-    app.get(`/${budgetInsertion}/:budgetCodes/:budgetAmbients`, function(req, res){
+    app.get(`/${budgetInsertion}/:budgetCodes/:budgetAmbients/:budgetInsertion`, function(req, res){
         //con.connect();
-        let sql = `Insert into Orcamento_comodos (aprovado, caminho, data, desconto, observacao, retificado, tipoCliente, valorTotal, arquiteto_id, clienteEmpresa_id, clienteEmpresaa_id,      clienteJuridico_id, pessoa_id, vendedor_id) values ${req.params.budgetAmbients}
+        let sql = `Insert into Orcamento_comodos (aprovado, caminho, data, desconto, observacao, retificado, tipoCliente, valorTotal, arquiteto_id, clienteEmpresa_id, clienteEmpresaa_id, clienteJuridico_id, pessoa_id, vendedor_id) values ${req.params.budgetInsertion}
                    Insert into Orcamento_comodos (Orcamento_id, comodos) values ${req.params.budgetAmbients};
                    Insert into Orcamento_codigos (Orcamento_id, codigos) values ${req.params.budgetCodes};
                     `;
