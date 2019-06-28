3dfc84d694ffd38a31662579fcb1c9173250d469
Jefferson <jeffersonrfmiranda@gmail.com>
2019-06-28 16:14:09 -0300

    make it better

--- app/routes/queryUpdateItem.js
+++ app/routes/queryUpdateItem.js
@@ -7,7 +7,7 @@ module.exports = function(app, updateItem){
     app.get(`/${updateItem}/:id/:description/:value`, function(req, res){
         
         console.log(req.params.nameEmpresa)
-        let sql = `UPDATE Item SET descricao= ${req.params.description},valorUnitario= ${req.params.description} WHERE id = ${req.params.id}`;
+        let sql = `UPDATE Item SET descricao= ${req.params.description},valorUnitario= ${req.params.value} WHERE id = ${req.params.id}`;
         
         con.query(sql, function(err, result){
             res.send(result);
