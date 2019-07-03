c3fef713585618c3fcebc26b140395a3ed941cb7
Jefferson <jeffersonrfmiranda@gmail.com>
2019-07-03 17:22:44 -0300

    make it better

--- app/routes/querySellerInsertion.js
+++ app/routes/querySellerInsertion.js
@@ -3,7 +3,7 @@ var dbConnection = require('../../config/dbConnection');
 module.exports = function(app, sellerInsertion){
     var con = dbConnection();
      app.get(`/${sellerInsertion}/:clientCel1/:clientCel2/:clientEmail/:clientName/:clientTel1/:clientTel2/:storeName`, function(req, res){
-        let sql = `INSERT INTO Vendedor (celular, celular2, email, nome, telefone, telefone2) VALUES (${req.params.clientCel1}, ${req.params.clientCel2}, ${req.params.clientEmail}, ${req.params.clientName}, ${req.params.clientTel1}, ${req.params.clientTel2},(SELECT id from Cliente where Cliente.nome =  ${req.params.storeName} and Cliente.DTYPE = 'ClienteJuridico'));
+        let sql = `INSERT INTO Vendedor (celular, celular2, email, nome, telefone, telefone2, empresa_id) VALUES (${req.params.clientCel1}, ${req.params.clientCel2}, ${req.params.clientEmail}, ${req.params.clientName}, ${req.params.clientTel1}, ${req.params.clientTel2},(SELECT id from Cliente where Cliente.nome =  ${req.params.storeName} and Cliente.DTYPE = 'ClienteJuridico'));
         SELECT LAST_INSERT_ID() INTO @ID;
         INSERT INTO Cliente_Vendedor (Cliente_id, vendedores_id) VALUES ((SELECT id from Cliente where Cliente.nome =  ${req.params.storeName} and Cliente.DTYPE = 'ClienteJuridico'), @ID)`;
          
