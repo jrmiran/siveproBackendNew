08cd92a2217472c275da7bf3a9c754d9908d0a25
Jefferson <jeffersonrfmiranda@gmail.com>
2019-07-11 11:02:36 -0300

    make it better

--- app/routes/queryBudgetUpdate.js
+++ app/routes/queryBudgetUpdate.js
@@ -5,18 +5,18 @@ module.exports = function(app, budgetUpdate){
     
     app.get(`/${budgetUpdate}/:budgetId/:discount/:note/:retified/:amount/:budgetCodes/:budgetAmbients/:budgetDetails/:budgetItems/:budgetMeasures/:budgetNeedings/:budgetNumbers/:budgetQuantitys/:budgetValues`, function(req, res){
         //con.connect();
-        let sql = ` UPDATE Orcamento SET desconto = ${req.params.discount}, observacao = ${req.params.note}, retificado = ${req.params.retified}, valotTotal = ${req.params.amount} WHERE Orcamento.id = ${req.params.budgetId};
-                    DELETE * FROM Orcamento_comodos as o WHERE o.orcamento_id = ${req.params.budgetId};
-                    DELETE * FROM Orcamento_codigos as o WHERE o.orcamento_id = ${req.params.budgetId};
-                    DELETE * FROM Orcamento_detalhes as o WHERE o.orcamento_id = ${req.params.budgetId};
-                    DELETE * FROM Orcamento_itens as o WHERE o.orcamento_id = ${req.params.budgetId};
-                    DELETE * FROM Orcamento_medidas as o WHERE o.orcamento_id = ${req.params.budgetId};
-                    DELETE * FROM Orcamento_necessidades as o WHERE o.orcamento_id = ${req.params.budgetId};
-                    DELETE * FROM Orcamento_numeros as o WHERE o.orcamento_id = ${req.params.budgetId};
-                    DELETE * FROM Orcamento_quantidades as o WHERE o.orcamento_id = ${req.params.budgetId};
-                    DELETE * FROM Orcamento_valores as o WHERE o.orcamento_id = ${req.params.budgetId};
+        let sql = ` UPDATE Orcamento SET desconto = ${req.params.discount}, observacao = ${req.params.note}, retificado = ${req.params.retified}, valorTotal = ${req.params.amount} WHERE Orcamento.id = ${req.params.budgetId};
+                    DELETE FROM Orcamento_comodos as o WHERE o.orcamento_id = ${req.params.budgetId};
+                    DELETE FROM Orcamento_codigos as o WHERE o.orcamento_id = ${req.params.budgetId};
+                    DELETE FROM Orcamento_detalhes as o WHERE o.orcamento_id = ${req.params.budgetId};
+                    DELETE FROM Orcamento_itens as o WHERE o.orcamento_id = ${req.params.budgetId};
+                    DELETE FROM Orcamento_medidas as o WHERE o.orcamento_id = ${req.params.budgetId};
+                    DELETE FROM Orcamento_necessidades as o WHERE o.orcamento_id = ${req.params.budgetId};
+                    DELETE FROM Orcamento_numeros as o WHERE o.orcamento_id = ${req.params.budgetId};
+                    DELETE FROM Orcamento_quantidades as o WHERE o.orcamento_id = ${req.params.budgetId};
+                    DELETE FROM Orcamento_valores as o WHERE o.orcamento_id = ${req.params.budgetId};
                     
-                    Insert into Orcamento_comodos (Orcamento_id, comodos) values ${req.params.budgetCodes};
+                    Insert into Orcamento_comodos (Orcamento_id, comodos) values ${req.params.budgetAmbients};
                     Insert into Orcamento_codigos (Orcamento_id, codigos) values ${req.params.budgetCodes};
                     Insert into Orcamento_detalhes (Orcamento_id, detalhes) values ${req.params.budgetDetails};
                     Insert into Orcamento_itens (Orcamento_id, itens) values ${req.params.budgetItems};
