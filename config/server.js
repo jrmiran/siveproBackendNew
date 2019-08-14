29f3cb682b476d98297011fa904c198d6b8a573e
Jefferson <jeffersonrfmiranda@gmail.com>
2019-08-14 11:26:13 -0300

    make it better

--- config/server.js
+++ config/server.js
@@ -1,11 +1,25 @@
 var express = require('express');
 var app = express();
+
+function ignoreFavicon(req, res, next) {
+  if (req.originalUrl === '/favicon.ico') {
+    res.status(204).json({nope: true});
+  } else {
+    next();
+  }
+}
+
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+app.use(ignoreFavicon);
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
+
+
+
 
 module.exports = app;
