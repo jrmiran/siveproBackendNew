b22475849bb0c9c5660f29dd854c3263aac88eb6
Jefferson <jeffersonrfmiranda@gmail.com>
2019-08-26 16:16:34 -0300

    make it better

--- app/routes/queryInsertSOExecution.js
+++ app/routes/queryInsertSOExecution.js
@@ -1,16 +1,14 @@
 var dbConnection = require('../../config/dbConnection');
 
-
 module.exports = function(app, soExecution){
     var con = dbConnection();
     
-    app.get(`/${soExecution}/:employees/:shares`, function(req, res){
+    app.get(`/${soExecution}/:osId/:date/:stone/:empreita/:stoneValue/:employees/:shares`, function(req, res){
         let sql =   `INSERT INTO OrdemServico_funcionarios(OrdemServico_id, funcionarios) VALUES ${req.params.employees};
-INSERT INTO OrdemServico_porcentagens(OrdemServico_id, porcentagens) VALUES ${req.params.shares}`;
+                     INSERT INTO OrdemServico_porcentagens(OrdemServico_id, porcentagens) VALUES ${req.params.shares};
+                     UPDATE OrdemServico SET dataTermino = ${req.params.date}, pedra = ${req.params.stone}, empreita = ${req.params.empreita}, terminado = 1, valorPedra = ${req.params.stoneValue} WHERE OrdemServico.id = ${req.params.osId}`;
         con.query(sql, function(err, result){
             res.send(result);
         });
     });
-    
-    
 }
