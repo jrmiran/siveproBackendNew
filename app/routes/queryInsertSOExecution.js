1011f5ec1d35a5587fe68d338c2109ecfa639b70
Jefferson <jeffersonrfmiranda@gmail.com>
2019-08-26 12:56:36 -0300

    make it better

--- app/routes/queryInsertSOExecution.js
+++ app/routes/queryInsertSOExecution.js
@@ -5,8 +5,8 @@ module.exports = function(app, soExecution){
     var con = dbConnection();
     
     app.get(`/${soExecution}/:employees/:shares`, function(req, res){
-        let sql =   `INSERT INTO OrdemServico_porcentagens(OrdemServico_id, porcentagens) VALUES ${req.params.shares};
-                     INSERT INTO OrdemServico_funcionarios(OrdemServico_id, funcionarios) VALUES ${req.params.employees}`;
+        let sql =   `INSERT INTO OrdemServico_funcionarios(OrdemServico_id, funcionarios) VALUES ${req.params.employees};
+INSERT INTO OrdemServico_porcentagens(OrdemServico_id, porcentagens) VALUES ${req.params.shares}`;
         con.query(sql, function(err, result){
             res.send(result);
         });
