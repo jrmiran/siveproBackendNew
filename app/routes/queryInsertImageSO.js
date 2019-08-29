33fcdece36dcbca146cc0864c0ce6d610fccb93b
Jefferson <jeffersonrfmiranda@gmail.com>
2019-08-29 10:45:20 -0300

    make it better

--- app/routes/queryInsertImageSO.js
+++ app/routes/queryInsertImageSO.js
@@ -4,7 +4,7 @@ module.exports = function(app, insertImageSO){
     var con = dbConnection();
     
     app.get(`/${insertImageSO}/:idSO/:imageUrl`, function(req, res){
-        let sql = ` INSERT INTO OrdemServico (imagem) VALUES (${req.params.imageUrl}) WHERE OrdemServico.id = ${req.params.idSO}`;
+        let sql = ` UPDATE OrdemServico SET imagem = ${req.params.imageUrl} WHERE OrdemServico.id = ${req.params.idSO}`;
         con.query(sql, function(err, result){
             res.send(result);
         });
