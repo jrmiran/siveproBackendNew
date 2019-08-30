05b9ae90ff52a42d309af5dc4fb6eab03f6ba3d8
Jefferson <jeffersonrfmiranda@gmail.com>
2019-08-30 10:26:50 -0300

    make it better

--- app/routes/postTest.js
+++ app/routes/postTest.js
@@ -15,7 +15,7 @@ module.exports = function(app, postTest){
     
     app.post(`/${postTest}`, function(req, res){
         //let sql = `SELECT * FROM Usuario WHERE Usuario.id = '${req.body.texto}'` ;
-        let sql = ` UPDATE OrdemServico SET imagem = ${req.body.image} WHERE OrdemServico.id = ${req.body.idSO}`;
+        let sql = ` UPDATE OrdemServico SET imagem = '${req.body.image}' WHERE OrdemServico.id = ${req.body.idSO}`;
         con.query(sql, function(err, result){
             //var obj = {query: JSON.stringify(result)};
             res.send(result);
