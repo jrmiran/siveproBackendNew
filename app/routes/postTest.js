9be3b668f362f40698d6e48145aea278b5e08acb
Jefferson <jeffersonrfmiranda@gmail.com>
2019-08-30 09:38:59 -0300

    make it better

--- app/routes/postTest.js
+++ app/routes/postTest.js
@@ -16,7 +16,8 @@ module.exports = function(app, postTest){
     app.post(`/${postTest}`, function(req, res){
         let sql = `SELECT * FROM Usuario WHERE Usuario.id = ${req.body['texto']}` ;
         con.query(sql, function(err, result){
-            res.send(result);
+            var obj = {query: result}
+            res.send(obj);
         });
     });
 }
