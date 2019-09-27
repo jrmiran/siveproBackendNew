239063ca48f053e8e19559f1689bb440cbcb39c4
Jefferson <jeffersonrfmiranda@gmail.com>
2019-09-27 07:54:30 -0300

    make it better

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -42,6 +42,18 @@ var mysql = require('mysql');
     });
 }*/
 
+/*module.exports = function(){
+    return mysql.createConnection({
+        connectionLimit : 25,
+        host: "mysql669.umbler.com",
+        user: "belartte",
+        port: 41890,
+        password: "mdcjjh13",  
+        database: "belartte",
+        multipleStatements: true
+    });
+}*/
+
 module.exports = function(){
     return mysql.createConnection({
         connectionLimit : 25,
