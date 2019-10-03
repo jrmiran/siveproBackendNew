0c17b64b32206eb274a0493779d9ee0b7371ee7c
Jefferson <jeffersonrfmiranda@gmail.com>
2019-10-03 16:53:09 -0300

    make it better

--- app/routes/keepConnected.js
+++ app/routes/keepConnected.js
@@ -5,7 +5,6 @@ module.exports = function(app, l, q){
     var con = dbConnection();
     
     app.get(l, function(req, res){  
-        //con.connect();
         setInterval(function () { con.query(q); }, 5000);
         //con.end();
     });
