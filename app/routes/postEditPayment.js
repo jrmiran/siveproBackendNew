e5eef7c86090b8a754296eb54d1d5763e7b178f6
Jefferson <jeffersonrfmiranda@gmail.com>
2019-11-01 09:43:09 -0300

    make it better

--- app/routes/postEditPayment.js
+++ app/routes/postEditPayment.js
@@ -4,7 +4,7 @@ module.exports = function(app, postInsertBudgetItems){
     var con = dbConnection();
     
     app.post(`/${postInsertBudgetItems}`, function(req, res){
-        let sql = `UPDATE Pagamento SET conta = '${req.body.bill}', data = '${req.body.date}', numeroCheque = '${req.body.check}', status = '${req.body.status}', valor = '${req.body.value}', formaPagamento_formaPagamento = '${req.body.paymentForm}', observacao = '${req.body.note}', tipoPagamento_tipoPagamento = '${req.body.paymentType}'`;
+        let sql = `UPDATE Pagamento SET conta = '${req.body.bill}', data = '${req.body.date}', numeroCheque = '${req.body.check}', status = '${req.body.status}', valor = '${req.body.value}', formaPagamento_formaPagamento = '${req.body.paymentForm}', observacao = '${req.body.note}', tipoPagamento_tipoPagamento = '${req.body.paymentType}' WHERE Pagamento.id = ${req.body.id}`;
         con.query(sql, function(err, result){
             res.send(result);
         });
