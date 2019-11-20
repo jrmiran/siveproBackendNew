27bcda48fe55e0df870cb198574b08cb0534bb87
Jefferson <jeffersonrfmiranda@gmail.com>
2019-11-20 17:07:37 -0300

    make it better

--- app/routes/postInsertDraw.js
+++ app/routes/postInsertDraw.js
@@ -4,7 +4,7 @@ module.exports = function(app, postInsertDraw){
     var con = dbConnection();
     
     app.post(`/${postInsertDraw}`, function(req, res){
-        let sql = `INSERT INTO Desenho (aprovado, imagem, orcamento_id, materia_id, ambiente) VALUES ${req.body.query}`;
+        let sql = `INSERT INTO Desenho (aprovado, imagem, orcamento_id, materia_id, ambiente, local) VALUES ${req.body.query}`;
         con.query(sql, function(err, result){
             res.send(result);
         });
