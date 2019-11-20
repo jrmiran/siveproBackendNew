27c229101ee453cba13a89559446fbdb3a5167e8
Jefferson <jeffersonrfmiranda@gmail.com>
2019-11-20 10:34:48 -0300

    make it better

--- app/routes/postSearchProjectByBudget.js
+++ app/routes/postSearchProjectByBudget.js
@@ -4,7 +4,7 @@ module.exports = function(app, postSearchProjectByBudget){
     var con = dbConnection();
     
     app.post(`/${postSearchProjectByBudget}`, function(req, res){
-        let sql = `SELECT  d.id, c.nome as store, ce.nome as client, o.id as budget, d.ambiente as a from Desenho as d JOIN Cliente as c JOIN ClienteEmpresa as ce JOIN Orcamento as o ON o.id = d.orcamento_id AND o.clienteEmpresaa_id = ce.id AND o.clienteJuridico_id = c.id WHERE o.id = ${req.body.budgetId} ORDER BY d.id`;
+        let sql = `SELECT  d.id, c.nome as store, ce.nome as client, o.id as budget, d.ambiente as ambient, m.nome as material from Desenho as d JOIN Cliente as c JOIN ClienteEmpresa as ce JOIN Orcamento as o ON o.id = d.orcamento_id AND o.clienteEmpresaa_id = ce.id AND o.clienteJuridico_id = c.id AND m.id = d.materia_id WHERE o.id = ${req.body.budgetId} ORDER BY d.id`;
         con.query(sql, function(err, result){
             res.send(result);
         });
