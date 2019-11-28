2f854d67dc0341a16aedcb8f248dabb5b715df81
Jefferson <jeffersonrfmiranda@gmail.com>
2019-11-28 10:09:20 -0300

    make it better

--- app/routes/queryClientBudget.js
+++ app/routes/queryClientBudget.js
@@ -7,7 +7,7 @@ module.exports = function(app, budgetClient){
     app.get(`/${budgetClient}/:nameEmpresa/:nameThirdy`, function(req, res){
         //con.connect();
         console.log(req.params.nameEmpresa)
-        let sql = `Select ce.nome, ce.telefone, ce.celular, ce.email, ce.endereco, c.id as id_Client, ce.id as id_Thirdy ce.cpf as thirdyCpf, ce.cnpj as thirdyCnpj from Cliente as c, ClienteEmpresa as ce where ce.nome = ${req.params.nameThirdy} and c.nome = ${req.params.nameEmpresa}`
+        let sql = `Select ce.nome, ce.telefone, ce.celular, ce.email, ce.endereco, c.id as id_Client, ce.id as id_Thirdy, ce.cpf as thirdyCpf, ce.cnpj as thirdyCnpj from Cliente as c, ClienteEmpresa as ce where ce.nome = ${req.params.nameThirdy} and c.nome = ${req.params.nameEmpresa}`
         
         con.query(sql, function(err, result){
             res.send(result);
