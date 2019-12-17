3fed1e413f1b0ac055742ed2713b421eb1dabc61
Jefferson <jeffersonrfmiranda@gmail.com>
2019-12-17 07:41:02 -0300

    make it better

--- app/routes/queryInsertion.js
+++ app/routes/queryInsertion.js
@@ -27,8 +27,10 @@ module.exports = function(app, budgetInsertionTest){
     
     app.get(`/${budgetInsertionTest}/:budgetInsertion`, function(req, res){
         //con.connect();
+        console.log(req.params.budgetInsertion);
         let sql = `Insert into Orcamento (aprovado, caminho, data, desconto, observacao, retificado, tipoCliente, valorTotal, arquiteto_id, clienteJuridico_id, clienteEmpresaa_id, clienteEmpresa_id, pessoa_id, vendedor_id, poload) values ${req.params.budgetInsertion};`;
         con.query(sql, function(err, result){
+            console.log(result);
             res.send(result);
             //con.end();
             //con.release();
