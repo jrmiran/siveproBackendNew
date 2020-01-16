054207b8a61b8d2dab709007985c442a8e77c5ef
Jefferson <jeffersonrfmiranda@gmail.com>
2020-01-16 15:54:44 -0300

    make it better

--- app/routes/postEditPayment.js
+++ app/routes/postEditPayment.js
@@ -4,7 +4,7 @@ module.exports = function(app, postInsertBudgetItems){
     var con = dbConnection();
     
     app.post(`/${postInsertBudgetItems}`, function(req, res){
-        let sql = `UPDATE Pagamento SET conta = '${req.body.bill}', data = '${req.body.date}', numeroCheque = '${req.body.check}', status = '${req.body.status}', valor = '${req.body.value}', formaPagamento_formaPagamento = '${req.body.paymentForm}', observacao = '${req.body.note}', tipoPagamento_tipoPagamento = '${req.body.paymentType}' , entrada = ${req.body.inOut} WHERE Pagamento.id = ${req.body.id}`;
+        let sql = `UPDATE Pagamento SET conta = '${req.body.bill}', data = '${req.body.date}', numeroCheque = '${req.body.check}', status = '${req.body.status}', valor = '${req.body.value}', formaPagamento_formaPagamento = '${req.body.paymentForm}', observacao = '${req.body.note}', tipoPagamento_tipoPagamento = '${req.body.paymentType}' , entrada = ${req.body.inOut}, orcamento_id = ${req.body.budgetId} WHERE Pagamento.id = ${req.body.id}`;
         con.query(sql, function(err, result){
             res.send(result);
         });
