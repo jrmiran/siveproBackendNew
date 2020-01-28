4808d3af3156ab526637970ea208f0c8ffb09795
Jefferson <jeffersonrfmiranda@gmail.com>
2020-01-28 09:44:06 -0300

    make it better

--- app/routes/postServiceOrderByEmployee.js
+++ app/routes/postServiceOrderByEmployee.js
@@ -4,7 +4,7 @@ module.exports = function(app, postServiceOrderByEmployee){
     var con = dbConnection();
     app.post(`/${postServiceOrderByEmployee}`, function(req, res){
         //let sql = `SELECT o.id, o.dataTermino, of.funcionarios, op.porcentagens FROM OrdemServico_funcionarios as of INNER JOIN OrdemServico_porcentagens as op INNER JOIN OrdemServico as o ON of.OrdemServico_id = op.OrdemServico_id WHERE o.id = of.OrdemServico_id AND o.id = op.OrdemServico_id`;
-        let sql = `SELECT * FROM OrdemServico_funcionarios; SELECT * FROM OrdemServico_porcentagens;SELECT id, valor, dataTermino, empreita, pedra, valorEmpreita, valorPedra FROM OrdemServico;`
+        let sql = `SELECT * FROM OrdemServico_funcionarios; SELECT * FROM OrdemServico_porcentagens;SELECT id, valor, dataTermino, empreita, pedra, valorEmpreita, valorPedra FROM OrdemServico;SELECT * FROM Funcionario;`
         con.query(sql, function(err, result){
             var response = [];
             result[0].forEach(function(data, index){
@@ -14,7 +14,8 @@ module.exports = function(app, postServiceOrderByEmployee){
                           dataTermino: result[2].find((v) => {return v.id == data['OrdemServico_id']})['dataTermino'], 
                           valor: result[2].find((v) => {return v.id == data['OrdemServico_id']})['valor'],
                           empreita: result[2].find((v) => {return v.id == data['OrdemServico_id']})['valorEmpreita'],  
-                          pedra: result[2].find((v) => {return v.id == data['OrdemServico_id']})['valorPedra']
+                          pedra: result[2].find((v) => {return v.id == data['OrdemServico_id']})['valorPedra'],
+                          funcao: result[3].find((v) => {return v.nome == data['funcionarios']})['funcao_funcao']               
                          })
             });
             res.send(response);
