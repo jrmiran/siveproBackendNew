5359dcad434d62f745852ff8911895d430e5a2e1
Jefferson <jeffersonrfmiranda@gmail.com>
2020-02-14 14:50:42 -0300

    make it better

--- app/routes/postSearchAllRequests.js
+++ app/routes/postSearchAllRequests.js
@@ -4,7 +4,7 @@ module.exports = function(app, postSearchAllRequests){
     var con = dbConnection();
     
     app.post(`/${postSearchAllRequests}`, function(req, res){
-        let sql = `SELECT * FROM Pedido; SELECT * FROM Pedido_orcamentos; SELECT * FROM Pedido_clientesEmpresa; SELECT * FROM Pedido_pagamentos;`;
+        let sql = `SELECT * FROM Pedido`;
         con.query(sql, function(err, result){
             res.send(result);
         });
