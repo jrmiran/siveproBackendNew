8dfcd5d6add829c1255f8f101d18ab3a3e8fbda6
Jefferson <jeffersonrfmiranda@gmail.com>
2020-02-14 11:46:56 -0300

    make it better

--- app/routes/postSearchBudgetByStoreId.js
+++ app/routes/postSearchBudgetByStoreId.js
@@ -4,7 +4,7 @@ module.exports = function(app, postSearchBudgetByStoreId){
     var con = dbConnection();
     
     app.post(`/${postSearchBudgetByStoreId}`, function(req, res){
-        let sql = `SELECT id, data, desconto, valorTotal, clienteEmpresaa_id, vendedor_id FROM Orcamento WHERE clienteJuridico_id = ${req.body.id}`;
+        let sql = `SELECT o.id, o.data, o.desconto, o.valorTotal, o.clienteEmpresaa_id, o.vendedor_id, ce.nome as nomeCliente, v.nome as nomeVendedor FROM Orcamento o, ClienteEmpresa ce, Vendedor as v WHERE o.clienteJuridico_id = ${req.body.id} AND ce.id = o.clienteEmpresaa_id and o.vendedor_id = v.id`;
         con.query(sql, function(err, result){
             res.send(result);
         });
