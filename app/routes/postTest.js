aaedd3666b1d91c158025cdb3d7818cc3e48b7b6
Jefferson <jeffersonrfmiranda@gmail.com>
2020-03-05 14:35:43 -0300

    make it better

--- app/routes/postTest.js
+++ app/routes/postTest.js
@@ -3,11 +3,6 @@ var dbConnection = require('../../config/dbConnection');
 module.exports = function(app, postTest){
     var con = dbConnection();
     
-    /*app.get(`/${budget}/:id`, function(req, res){
-        let sql = `SELECT * FROM Orcamento WHERE Orcamento.id = ${req.params.id}`;
-        
-    });*/
-    
     
     app.post(`/${postTest}`, function(req, res){
         let sql = ` UPDATE OrdemServico SET imagem = '${req.body.image}' WHERE OrdemServico.id = ${req.body.idOs}`;
