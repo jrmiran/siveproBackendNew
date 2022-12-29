e2b1c9a78059a557baed26ffafc74c8898ec377a
jrmiran <46909195+jrmiran@users.noreply.github.com>
2022-12-29 15:16:08 -0300

    Update dbConnection.js

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,8 +2,17 @@ var mysql = require('mysql');
 
 module.exports = function(){
     
-    
     return mysql.createConnection({
+        connectionLimit : 25,
+        host: "br954.hostgator.com.br",
+        user: "belart72_belartte",
+        port: 2083,
+        password: "Mdcjjh13!@",  
+        database: "belart72_belartte",
+        multipleStatements: true
+    });
+    
+    /*return mysql.createConnection({
         connectionLimit : 25,
         host: "mysql669.umbler.com",
         user: "belartte",
@@ -11,7 +20,7 @@ module.exports = function(){
         password: "mdcjjh13",  
         database: "belartte",
         multipleStatements: true
-    });
+    });*/
     
     
 }
